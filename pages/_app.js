aa5a70f5e39b319922d022fc437b2e7c0dc3f310
Marcos Gabriel <125589443+MarcosGAC@users.noreply.github.com>
2023-03-07 16:52:01 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import Navbar from '@/src/components/navbar'
+import Head from 'next/head'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return( 
     <>
+    <Head>
+        <title>Portifolio Next</title>
+      </Head>
     <Navbar />
   <Component {...pageProps} />
   </>)
